05f3c4c72cc648b1fdb55a267a59cb8f756ee5fd
Olgun Cengiz <olgun.cengiz@ericsson.com>
2018-01-31 03:17:50 +0300

    fetching gallery from Flickr and showing ids

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import logging
 import requests
 import os
 from flask import Flask, render_template, send_from_directory, request, json, jsonify
+from flask.ext.triangle import Triangle
 from logging.handlers import RotatingFileHandler
 
 application = Flask(__name__)
+Triangle(application)
 
 @application.before_first_request
 def setup_logging():
@@ -44,10 +46,11 @@ def getImageList():
         url = 'https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=7085acc211b37cf566b6abf9e5efd5a3&format=json&nojsoncallback=1&extras=description%2C+license%2C+date_upload%2C+date_taken%2C+owner_name%2C+icon_server%2C+original_format%2C+last_update%2C+geo%2C+tags%2C+machine_tags%2C+o_dims%2C+views%2C+media%2C+path_alias%2C+url_sq%2C+url_t%2C+url_s%2C+url_q%2C+url_m%2C+url_n%2C+url_z%2C+url_c%2C+url_l%2C+url_o&gallery_id='
         url = url + gallery_id
         response = requests.get(url, verify=False)
-        resp_dict = response.json()
+        #resp_dict = response.json()
         #application.logger.info('Response.Text: %s' % response.text)
-        application.logger.info('Response: %s' % resp_dict)
-        return jsonify(resp_dict)
+        application.logger.info('Response: %s' % response.json())
+        #return jsonify(resp_dict)
+        return jsonify(response.json())
 
     except Exception,e:
         return jsonify(status='ERROR',message=str(e))
--- static/js/main.js
+++ static/js/main.js
@@ -5,20 +5,38 @@
 
   .controller('FlickrController', ['$scope', '$log', '$http',
 	function($scope, $log, $http) {
+		$scope.photos = [{"id": "1"}, {"id": "2"}, {"id": "3"}];
+		
 		$scope.getPhotos = function() {
-			$log.log("test");
+			$log.log("Inside Angular Function");
 
 			// get the gallery id from the input
 			var galleryId = $scope.galleryId;
 			$log.log("Gallery ID:" + galleryId);
 
-			// fire the API request
+			/*// fire the API request
 			$http.get('/getImageList', {params: {"galleryId": galleryId}}).
 				success(function(results) {
+					$scope.photos = results.data;
 					$log.log(results);
 				}).
 				error(function(error) {
 					$log.log(error);
+			});*/
+			$http({
+				url: '/getImageList',
+				dataType: 'json',
+				method: 'GET',
+				params: {"galleryId": galleryId},
+				headers: {
+					"Content-Type": "application/json"
+				}
+
+			}).success(function(response){
+				$scope.photos = response.photos.photo;
+				$log.log(response.photos.photo);
+			}).error(function(error){
+				$log.log(error);
 			});
 		};
 	}
